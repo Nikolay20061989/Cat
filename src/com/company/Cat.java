b7270781339120e1a0f3e0114d8179a2c612b9e6
Admin <ivashenko.n77@gmail.com>
2020-11-16 22:09:01 +0200

    Commit 1

--- src/com/company/Cat.java
+++ src/com/company/Cat.java
@@ -1,23 +1,52 @@
 package com.company;
 
 public class Cat {
-private String name;
-private String color;
-private int age;
+    private String name;
+    private String color;
+    private int age;
 
     public Cat(String name, String color, int age) {
         this.name = name;
         this.color = color;
         this.age = age;
     }
-public void jump(){
-    System.out.println(name +" to jump");
-}
-public void sleep(){
-    System.out.println(name +" to sleep");
-    }
-public void eat(){
-    System.out.println(name +"  eats");
-}
 
+    public void jump() {
+        System.out.println(name + " to jump");
+    }
+
+    public void sleep() {
+        System.out.println(name + " to sleep");
+    }
+
+    public void eat() {
+        System.out.println(name + "  eats");
+    }
+
+    public Cat() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
